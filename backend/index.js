6f027698b04be838beb0725970e7281ec2d50390
tuanne <nguyenhoangminhtuan210802@gmail.com>
2024-08-01 14:16:08 +0700

    Initial setup

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+const uri = process.env.MONGO_URI;
+
+// Connect to MongoDB
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected...'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
